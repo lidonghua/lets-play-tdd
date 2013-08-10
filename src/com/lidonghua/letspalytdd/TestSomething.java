57d2fcf5a293377e9d7d207248f9879cc91964f3
Li Donghua <donghua.li@dianping.com>
2013-08-11 01:17:04 +0800

    Then make it passed.

--- src/com/lidonghua/letspalytdd/TestSomething.java
+++ src/com/lidonghua/letspalytdd/TestSomething.java
@@ -2,11 +2,8 @@ package com.lidonghua.letspalytdd;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
-
 public class TestSomething {
     @Test
     public void nothing() {
-        fail("Hi");
     }
 }
