859272dcbba2ca6739f367dbdda8bf7f1f988477
Donghua Li <me@lidonghua.com>
2013-08-11 01:12:39 +0800

    TDD begins with a failed test.

---
+++ src/com/lidonghua/letspalytdd/TestSomething.java
@@ -0,0 +1,12 @@
+package com.lidonghua.letspalytdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class TestSomething {
+    @Test
+    public void nothing() {
+        fail("Hi");
+    }
+}
